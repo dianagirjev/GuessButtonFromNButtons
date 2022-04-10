f224cdaecb39546b5f3f16c6e03c090c4191d1e4
Girjev Diana <diana.girjev@gmail.com>
2022-04-10 22:45:02 +0300

    Commit_4

--- GuessButtonFromNButtons.js
+++ GuessButtonFromNButtons.js
@@ -3,18 +3,18 @@ function generateButtons() {
   let container = document.createElement("div");
   document.body.appendChild(container);
   container.className = "container";
-  container.innerHTML = '<div class="card text-center" id="card"></div>';
-  let card = document.getElementById("card");
-  card.innerHTML = '<div class="card-body" id="card_body"></div>';
-  let card_body = document.getElementById("card_body");
-  card_body.innerHTML = '<p>Choose only one button from this list with ' + n + ' buttons. </p>';
+  container.innerHTML = `<div class="card text-center" id="card">
+                          <div class="card-body" id="card_body">
+                            <p>Choose only one button from this list with ` + n + ` buttons. </p>
+                          </div>
+                        </div>`;
   let winningButton = generateWinningButton(n);
   for (let i = 0; i < n; ++i) {
     let button = document.createElement("button");
     button.className = "btn btn-primary";
     button.innerHTML = 'Button ' + (i + 1);
     button.setAttribute("onclick", "checkWinningButton(" + (i + 1) + "," + winningButton + ")");
-    card_body.appendChild(button);
+    document.getElementById("card_body").appendChild(button);
   }
   return false;
 }
